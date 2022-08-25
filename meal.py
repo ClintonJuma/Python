5b52b07d1a725d684c54f23f92bf84036a417efa
Clinton Juma <109604462+ClintonJuma@users.noreply.github.com>
2022-08-25 12:38:04 -0700

    Create meal.py

---
+++ meal.py
@@ -0,0 +1,13 @@
+import datetime
+from datetimerange import DateTimeRange
+time=int(input("What is the time: "))
+
+def main():
+    hours, minutes = time.split(":")
+    if time in range("7:00>=800"):
+        print("Breakfast time")
+    else:
+        pass
+main()
+def convert(time):
+    ...
